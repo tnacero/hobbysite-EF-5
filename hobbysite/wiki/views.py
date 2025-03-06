dfcc66f3b2b7c3de590a3d5804b61d89fbfaec2a
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-06 20:34:46 +0800

    Update views.py
    added ArticleListView and ArticleDetailView.

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/articles.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article/article_detail.html'
