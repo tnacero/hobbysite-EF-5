ea41ccd825d23808acd73bbf2ef7529e55364e36
hanashimi <crmacaspac@gmail.com>
2025-03-07 23:04:50 +0800

    [Documentation] Added comments to all of the blog app files

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -3,17 +3,31 @@ from django.urls import reverse
 
 
 class ArticleCategory(models.Model):
+    """Class for the categories of articles and their description
+
+       name: name of the category
+       description: description of the category
+    """
     name = models.CharField(max_length=255)
     description = models.TextField(blank=False)
     
-    def __str__(self):
+    def __str__(self): # Returns the title of a specific article
+    
         return '{}'.format(self.name)
 
-    class Meta:
-        ordering = ['name']
+    class Meta: 
+        ordering = ['name'] # Orders the name of the categories by ascending order
         verbose_name_plural = "categories"
     
 class Article(models.Model):
+    """Class for the specific details of the article
+
+        title: title of the article
+        category: category of the article
+        entry: entry of the user in regards to the article
+        created_on: date and time the article was created; automatic
+        updated_on: date and time the article was last updated; automatic
+    """
     title = models.CharField(max_length=255)
     category = models.ForeignKey(ArticleCategory, 
                                     on_delete=models.SET_NULL, 
@@ -23,12 +37,12 @@ class Article(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
-    def __str__(self):
+    def __str__(self): # Returns the title of a specific article
         return '{}'.format(self.title) 
     
-    def get_absolute_url(self):
+    def get_absolute_url(self): # Returns the url of a specific article
         return reverse("blog:article-detail", args=[self.pk])
     
-    class Meta:
-        ordering = ['-created_on']
-# Create your models here.
+    class Meta: 
+        ordering = ['-created_on'] # Orders the date of creation of the articles by descending order
+
--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -3,10 +3,12 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Article, ArticleCategory
 
-class ArticleListView(ListView):
-    model = ArticleCategory
-    template_name = 'articlelist.html'
+class ArticleListView(ListView): 
+    """Class for the list view of the articles"""
+    model = ArticleCategory 
+    template_name = 'articlelist.html' 
 
 class ArticleDetailView(DetailView):
-    model = Article
-    template_name = 'articles.html'
+    """Class for the detail view of the articles"""
+    model = Article 
+    template_name = 'articles.html' 
