8aabd6dfdc263e698a687d5e62925d62614f6add
okayroy <160197662+okayroy@users.noreply.github.com>
2025-03-07 23:31:31 +0800

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('forum/threads', PostListView.as_view() , name='post-list'),
-    path('forum/thread/<int:pk>', PostDetailView.as_view() , name='post-detail'),
+    path('threads', PostListView.as_view() , name='post-list'),
+    path('thread/<int:pk>', PostDetailView.as_view() , name='post-detail'),
 ]
 
 app_name = "forum"
