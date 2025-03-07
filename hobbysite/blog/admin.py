a559c1e6eaf817b1dc5b14b1cb4a72282e64beb7
hanashimi <crmacaspac@gmail.com>
2025-03-07 21:51:05 +0800

    [Cleaning Code] Removed Unnecessary Code, Modified Admin Panel

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Article, ArticleCategory
 
 class ArticleCategoriesInline(admin.TabularInline):
-    model = Article
+    model = ArticleCategory
 
 class ArticleAdmin(admin.ModelAdmin):
+    model = Article
     inlines = [ArticleCategoriesInline,]
     
-admin.site.register(ArticleCategory, ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory)
--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('blog.urls', namespace="blog")),
+    path('blog/', include('blog.urls', namespace="blog")),
     path('admin/', admin.site.urls),
 ]
