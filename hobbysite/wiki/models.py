3600bf65d9581cc185754c2b40f7128dcc330b7e
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-07 21:32:16 +0800

    Update models.py

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -11,11 +11,8 @@ class ArticleCategory(models.Model):
         return self.name
         
     def __str__(self):
-        """returning description text field."""
         return self.description
         
-    def get_absolute_url(self):
-        return reverse('article_category_detail', args=[str(self.pk)])
     
     class Meta:
         ordering = ['name']
@@ -25,24 +22,24 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name = 'article')
+    category = models.ForeignKey(ArticleCategory, 
+                                     on_delete=models.SET_NULL, 
+                                     null=True, 
+                                     related_name = 'article')
     entry = models.TextField()    
-    created_on = models.DateTimeField(auto_now_add=True, null=False)
-    updated_on = models.DateTimeField(auto_now=True, null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
         
     def __str__(self):
-        """returning category text field."""
         return self.category
     
     def __str__(self):
-        """returning date created."""
         return self.created_on
     
     def __str__(self):
-        """returning date updated."""
         return self.updated_on
         
     def get_absolute_url(self):
