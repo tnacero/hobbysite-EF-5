27e0f345083737a0563c57ba4321688f7ca2d5ba
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-07 22:58:28 +0800

    Update admin.py
    Added admin panels.

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Article, ArticleCategory
 
-# Register your models here.
+
+class ArticleCategoriesInline(admin.TabularInline):
+    model = Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    inlines = [ArticleCategoriesInline,]
+
+
+admin.site.register(ArticleCategory, ArticleAdmin)
+admin.site.register(Article)
