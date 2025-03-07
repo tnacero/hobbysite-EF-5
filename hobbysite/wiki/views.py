2cfc2a4d56f55e695960dd054fc8f628176e1657
clavo-sk <sophia.clavo@student.ateneo.edu>
2025-03-07 23:15:24 +0800

    Added comments to views.py

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,4 @@
+"""This file sets up the views for the wiki app."""
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -7,10 +8,14 @@ from .models import Article, ArticleCategory
 
 
 class ArticleListView(ListView):
+    """Creates List View for the Article model."""
+    
     model = Article
     template_name = 'articles/articles.html'
 
 
 class ArticleDetailView(DetailView):
+    """Creates Detail View for the ArticleCategory model."""
+    
     model = ArticleCategory
     template_name = 'article/article_detail.html'
