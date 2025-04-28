f50416f5f9ed3e03b83f4084d8f185214c92354a
Nico Acero <trisha.acero@student.ateneo.edu>
2025-04-28 10:04:46 +0800

    Updated views.py

--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile
 from .forms import ProfileForm
 # Create your views here.
 
-class ProfileUpdateView(UpdateView):
+class ProfileUpdateView(UpdateView, LoginRequiredMixin):
     model = Profile
     form_class = ProfileForm
     template_name = 'profile_form.html'
