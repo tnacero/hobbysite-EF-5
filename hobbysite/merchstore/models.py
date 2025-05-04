cb5d8fdb8709c37a8d8fc4be358a3ac7da3af9a6
Wilfredo Luis N. Salvador <wilfredo.salvador@student.ateneo.edu>
2025-05-04 23:37:43 +0800

    updated models with Profile class in mind.

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 class ProductType(models.Model):
     """Class representing a product type."""
@@ -29,9 +30,22 @@ class Product(models.Model):
     product_type = models.ForeignKey( 
         ProductType, on_delete=models.SET_NULL, null=True,
         related_name='product',)
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True
+        )
     description = models.TextField()
     price = models.FloatField()
+    stock = models.IntegerField()
+    status = models.CharField()
 
+    def __str__(self):
+        """Check if this works later."""
+        if Product.stock < 1:
+            self.status = "Out of stock"
+            return self.status
+        else: 
+            return self.status
+    
     def __str__(self):
         """Function returning product name."""
         return self.name
@@ -46,3 +60,14 @@ class Product(models.Model):
     def get_absolute_url(self):
         """Function returning url of product details."""
         return reverse('merchstore:item-detail', args=[str(self.pk)])
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True
+    )
+    amount = models.IntegerField()
+    status = models.CharField()
+    created_on = models.DateTimeField()
