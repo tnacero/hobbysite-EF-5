ba92038119ea145efe11f1c648e4eb06bb6a865d
Wilfredo Luis N. Salvador <wilfredo.salvador@student.ateneo.edu>
2025-05-11 23:37:31 +0800

    Currently, nearly done with the creating products, need to update product list and product detail, and fixing transactions.

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,9 +1,13 @@
 """Module providing a function redirecting urls to merch store."""
 
 from django.urls import path
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail, ProductCreate, ProductUpdate, Cart, Transactions
 urlpatterns = [
     path('', ProductList.as_view(), name= 'items'),
+    path('add', ProductCreate.as_view(), name= 'add'),
+    path('edit', ProductUpdate.as_view(), name= 'edit'),
+    path('cart', Cart.as_view(), name= 'cart'),
+    path('transactions', Transactions.as_view(), name= 'transactions'),
     path('<int:pk>', ProductDetail.as_view(), name = "item-detail"),
 ]
 app_name = 'merchstore'
