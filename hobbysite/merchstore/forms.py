fe03e067ead84052393309dd4748a133e28545dd
Wilfredo Luis N. Salvador <wilfredo.salvador@student.ateneo.edu>
2025-05-11 20:18:17 +0800

    current progress on the merchstore final app concerning create views, update views, cart, transactions, and more.

---
+++ hobbysite/merchstore/forms.py
@@ -0,0 +1,26 @@
+"""
+Importing forms from Django with Recipe models for the forms.
+"""
+from django import forms
+from .models import Product, Transaction
+
+class ProductForm(forms.ModelForm) :
+    """Class for the ProductForm form."""
+    class Meta:
+        """Meta class for the fields utilized by ProductForm."""
+        model = Product
+        fields = "__all__"
+        widgets = {
+            "product_type":forms.Select()
+            ,"status":forms.Select()
+        }
+
+class TransactionForm(forms.ModelForm) :
+    """
+    Class for the TransactionForm form.
+    """
+    class Meta:
+        """Meta class for the fields utilized by Transaction"""
+        model = Transaction
+        fields = "__all__"
+        
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,7 +1,11 @@
 """Modules importing django views and Product from app models for url views."""
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Product
+from .forms import ProductForm, TransactionForm
 
 class ProductList(ListView):
     """Class viewing the list of products."""
@@ -14,3 +18,42 @@ class ProductDetail(DetailView):
 
     model = Product
     template_name = "item/item_detail.html"
+    form_class = TransactionForm
+
+class ProductCreate(LoginRequiredMixin, CreateView):
+    """
+    Class to create a new product.
+    """
+    model = Product
+    template_name = "item/add.html"
+    form_class = ProductForm
+    
+    def get_success_url(self):
+        return reverse_lazy('merchstore:items')
+    redirect_field_name = "registration/login.html"
+
+class ProductUpdate(LoginRequiredMixin, UpdateView):
+    """
+    Class to Update a product.
+    """
+    model = Product
+    template_name = "item/edit.html"
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse_lazy('merchstore:items')
+    redirect_field_name = "registration/login.html"
+
+class Cart(ListView):
+    """
+    This class is to view the current products available in the cart.
+    """
+    model = Product
+    template_name = "items/cart.html"
+
+class Transactions(ListView):
+    """
+    This class is to show the list of transactions currently available.
+    """
+    model = Product
+    template_name = "items/transactions.html"
