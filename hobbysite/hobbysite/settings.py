d919777e2d2fe3de248a98e6bba5fac831fc7132
okayroy <sarah.roy@student.ateneo.edu>
2025-05-14 14:34:19 +0800

    Updated forum app to allow uploading of static files

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -126,14 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = '/home'
-LOGOUT_REDIRECT_URL = '/accounts/login'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
