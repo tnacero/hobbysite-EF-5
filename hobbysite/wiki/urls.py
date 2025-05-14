4de2c7a02c4bf261d3db26958cb264159fd4212c
Sophia Clavo <sophia.clavo@student.ateneo.edu>
2025-05-14 22:49:40 +0800

    Added /add/ and /edit/ URLS

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,11 +1,13 @@
 """This file sets up the urls for the wiki app."""
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
 
     path('articles/', ArticleListView.as_view(), name='list'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail')
+    path('article/add', ArticleCreateView.as_view() , name='add'),
+    path('article/<int:pk>/edit', ArticleUpdateView.as_view() , name='edit'),
     
 ]
 
