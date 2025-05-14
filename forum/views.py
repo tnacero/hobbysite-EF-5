e3d09e477547dac28239383955ca4715143766f6
okayroy <sarah.roy@student.ateneo.edu>
2025-05-14 11:51:51 +0800

    Updated forum app to allow uploading of static images

--- forum/views.py
+++ forum/views.py
@@ -1,18 +1,24 @@
 """This file defines each view by referencing the models."""
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import PostCategory, Post
+from .models import ThreadCategory, Thread
 
 
-class PostListView(ListView):
-    """Views the posts' category."""
+class ThreadListView(ListView):
+    """Views the thread's category."""
 
-    model = PostCategory
+    model = ThreadCategory
     template_name = 'templates/thread_list.html'
 
 
-class PostDetailView(DetailView):
-    """Views the details of the thread of posts."""
+class ThreadDetailView(DetailView):
+    """Views the details of the thread of threads."""
 
-    model = Post
+    model = Thread
     template_name = 'templates/thread_detail.html'
+
+class ThreadCreateView():
+    
+
+class ThreadAddView():
+
