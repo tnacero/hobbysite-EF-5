ff3d952051a9d167e0b73d56cac3a536693c542e
Nico Acero <trisha.acero@student.ateneo.edu>
2025-05-15 09:34:38 +0800

    Update admin.py

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,21 +1,37 @@
 """This file sets up the admin panel for the commissioins app."""
 from django.contrib import admin
 
-from .models import Commission, Comment
+from .models import Commission, Job, JobApplication
 
 
-class CommentInline(admin.TabularInline):
-    """Creates an inline for the Comment model."""
+class JobInline(admin.TabularInline):
+    """Creates an inline for the Job model."""
 
-    model = Comment
+    model = Job
 
+class JobApplicationInline(admin.TabularInline):
+    """Creates an inline for the JobApplication model."""
+
+    model = JobApplication
 
 class CommissionAdmin(admin.ModelAdmin):
     """Creates the admin for the Commission model."""
 
     model = Commission
-    inlines = [CommentInline,]
+    inlines = [JobInline,]
+
+class JobAdmin(admin.ModelAdmin):
+    """Creates the admin for the Job model."""
+
+    model = Job
+    inlines = [JobApplicationInline,]
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    """Creates the admin for the JobApplication model."""
+
+    model = JobApplication
 
 
 admin.site.register(Commission, CommissionAdmin)
-admin.site.register(Comment)
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
