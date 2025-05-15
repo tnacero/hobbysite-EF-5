603f8debe04b5ae67786f75e03d6918853a51442
okayroy <sarah.roy@student.ateneo.edu>
2025-05-15 13:18:42 +0800

    Edited forum urls location

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -5,6 +5,8 @@ from .views import ThreadListView, ThreadDetailView
 urlpatterns = [
     path('forum/threads', ThreadListView.as_view() , name='post-list'),
     path('forum/thread/<int:pk>', ThreadDetailView.as_view() , name='post-detail'),
+    path('forum/thread/add', ThreadCreateView.as_view(), name='thread-create'),
+    path('forum/thread/<int:pk>/edit', ThreadAddView.as_view(), name='thread-add'),
 ]
 
 app_name = "forum"
