48bebd5c15d455dab4122528b0f7e01e93bcf55e
Sophia Clavo <sophia.clavo@student.ateneo.edu>
2025-05-15 17:52:19 +0800

    Attempting to add Comment Form
    Page refuses to load after comment is submitted

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -36,7 +36,7 @@ class Article(models.Model):
                                      null=True, 
                                      related_name = 'category')
     entry = models.TextField()    
-    header_image = models.ImageField(upload_to='static/img/', null=True)
+    header_image = models.ImageField(upload_to='static/img/', blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
